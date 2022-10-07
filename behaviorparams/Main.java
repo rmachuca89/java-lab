b7c3070da880636d6f6ad5457e0227498b331a4f
Rodrigo Machuca <rmachuca89@hotmail.com>
2022-10-07 16:16:06 -0500

    Add anonymous class example of behaviorparams

--- behaviorparams/Main.java
+++ behaviorparams/Main.java
@@ -23,10 +23,26 @@ public final class Main {
     apples.add(myApple4);
     apples.add(myApple5);
 
-    // Here we select the corresponding strategy to execute.
-    // final PrintStrategy ps = new SimplePrintStrategy();
-    final PrintStrategy ps = new PrettyPrintStrategy();
-    prettyPrintApples(apples, ps);
+    /** Here we select the corresponding strategy to execute. */
+    // prettyPrintApples(apples, new SimplePrintStrategy());
+    // prettyPrintApples(apples, new PrettyPrintStrategy());
+
+    /**
+     * Anonymous class example (DISCOURAGED).
+     *
+     * <p>It supports adhoc implementations with anonymous classes to attempt to make implementation
+     * less verbose by avoiding to having to declare a formal implementation class for each
+     * strategy. However, they are still bulky and create even more complexity by having to read the
+     * code body to understand what it does; and even worse, variable scoping may get tricky to get
+     * right.
+     */
+    prettyPrintApples(
+        apples,
+        new PrintStrategy() {
+          public void print(Apple a) {
+            System.out.println("Adhoc anonymous class strategy. Oh! yes, my apple: " + a);
+          }
+        });
   }
 
   static void prettyPrintApples(List<Apple> apples, PrintStrategy ps) {
