558749412855681d3a2e23844881bde36ddf9375
Rodrigo Machuca <rmachuca89@hotmail.com>
2023-11-04 21:27:59 -0500

    Add generics simple example

---
+++ generics/Foo.java
@@ -0,0 +1,8 @@
+package generics;
+
+class Foo {
+    public Foo() {
+        // I am the foo... and I do not do anything...
+        // custom class for illustrative purposes
+    }
+}
---
+++ generics/MyGenericClass.java
@@ -0,0 +1,13 @@
+package generics;
+
+class MyGenericClass<T> {
+    // My Generic Class.
+    // T parameter is arbitrary but is conventional as it hints "Type"
+    // E as in Element, K as en Key, V as in Value are also common.
+    T myValue;
+    public MyGenericClass(T value){
+        this.myValue = value;
+        System.out.println(myValue.getClass().getName());
+    }
+}
+
